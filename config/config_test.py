75cf1405cd8a4d01d7123ef02db8c96645fafe12
Danilo Moura <danilolmoura@gmail.com>
2020-08-15 04:41:27 -0300

    Use the same database for local and tests

--- config/config_test.py
+++ config/config_test.py
@@ -8,8 +8,8 @@ class ConfigTest(Config):
     TESTING = True
     FLASK_ENV = 'test'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
-        os.environ['POSTGRES_ZE_USER_API'],
-        os.environ['POSTGRES_ZE_PASSWORD_API'],
-        os.environ['POSTGRES_ZE_SERVER_API'],
+        os.environ['POSTGRES_ZE_USER_API_DEV'],
+        os.environ['POSTGRES_ZE_PASSWORD_API_DEV'],
+        os.environ['POSTGRES_ZE_SERVER_API_DEV'],
         os.environ['POSTGRES_ZE_DB_API_DEV'],
     )
